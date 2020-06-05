b1467e19d8b5c81be6cc9a5decb8d33e2fc65c2f
Thiago Felipe <thiagof.g3@gmail.com>
2020-06-04 23:21:46 -0300

    Adicionando duas linguagens frances e Portugues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import localeFr from '@angular/common/locales/fr';
-import localePt from '@angular/common/locales/pt';
-import { registerLocaleData } from '@angular/common';
-
-registerLocaleData(localeFr, 'fr-FR');
-registerLocaleData(localePt, 'pt-PT');
 
 @NgModule({
   declarations: [
