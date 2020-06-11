c7e2d17e61c95da013e97c4c79a9d536fcad9381
Thiago Felipe <thiagof.g3@gmail.com>
2020-06-10 21:52:59 -0300

    Adicionando idioma frances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Router, NavigationStart, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lojaFront';
+
+  constructor(
+    private translate: TranslateService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.translate.addLangs(['fr'])
+    this.translate.setDefaultLang('fr');
+  }
+
+  ngOnInit() {
+
+  }
+
+  mudarIdioma(lang: string) {
+    this.translate.use(lang);
+  }
 }
